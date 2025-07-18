1492cd9aa133b03eef0031ea6a9956e4ef0031f6
Mihail Kondratev <misereli@ya.ru>
2025-07-18 11:26:02 +0300

    Revert "4"
    This reverts commit 80c3702d24600b08bfd4870ee328385119db0bde.

--- internal/handler/start.go
+++ internal/handler/start.go
@@ -40,10 +40,10 @@ func (h Handler) StartCommandHandler(ctx context.Context, b *bot.Bot, update *mo
 			slog.Error("error activating trial", err)
 		}
 		existingCustomer.SubscriptionLink = &trial
-		slog.Info("trial link", trial)
+		slog.Info("trial link %s", trial)
 		expireAt := time.Now().AddDate(0, 0, config.TrialDays())
 		existingCustomer.ExpireAt = &expireAt
-		slog.Info("expired at", expireAt)
+		slog.Info("expired at %s", expireAt)
 
 		if strings.Contains(update.Message.Text, "ref_") {
 			arg := strings.Split(update.Message.Text, " ")[1]
@@ -171,7 +171,8 @@ func (h Handler) buildStartKeyboard(existingCustomer *database.Customer, langCod
 	inlineKeyboard = append(inlineKeyboard, [][]models.InlineKeyboardButton{{{Text: h.translation.GetText(langCode, "buy_button"), CallbackData: CallbackBuy}}}...)
 
 	if existingCustomer.SubscriptionLink != nil && existingCustomer.ExpireAt.After(time.Now()) {
-		inlineKeyboard = append(inlineKeyboard, h.resolveConnectButton(langCode))
+		url := *existingCustomer.SubscriptionLink
+		inlineKeyboard = append(inlineKeyboard, []models.InlineKeyboardButton{{Text: h.translation.GetText(langCode, "connect_button"), URL: url}})
 	}
 
 	if config.GetReferralDays() > 0 {
